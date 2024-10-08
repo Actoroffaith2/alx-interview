b30cf8f7047f80a84e34e8836b3b913731141eee
Actoroffaith2 <richardson4divinity57@gmail.com>
2024-10-08 06:41:27 +0000

    0x01-lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,33 +1,20 @@
 #!/usr/bin/python3
 """
-Lockboxes challenge
+Method to determine if all boxes can be opened
+Using prototype: def canUnlockAll(boxes)
 """
 
 
 def canUnlockAll(boxes):
     """
-    Deteremines if all the boxes can be opened.
-
-    Args:
-        boxes (list of lists): The lockboxes and their
-        respective keys
-
-    Returns:
-        bool: True if all boxes can be opened else False
+    Check if boxes can be unlocked
     """
-
-    box_count = len(boxes)
-
-    # Capture all unlocked boxes
-    unlocked = set()
-
-    for idx, box in enumerate(boxes):
-        if len(box) == 0 or idx == 0:
-            unlocked.add(idx)
-
-        for key in box:
-            if key < box_count and key != idx:
-                # Update unlocked boxes
-                unlocked.add(key)
-
-    return len(unlocked) == box_cou
+    for key in range(1, len(boxes) - 1):
+        ctr = False
+        for idx in range(len(boxes)):
+            ctr = (key in boxes[idx] and key != idx)
+            if ctr:
+                break
+        if ctr is False:
+            return ctr
+    return True

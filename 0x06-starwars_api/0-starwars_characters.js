d3fd6a5268e149009543430f528ff655bfcd85e6
Actoroffaith2 <richardson4divinity57@gmail.com>
2024-11-16 01:49:37 -0800

    0x06-starwars_api

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,28 +1,53 @@
 #!/usr/bin/node
+
 const request = require('request');
-const { argv } = require('process');
 
-const url = 'https://swapi-api.hbtn.io/api/films/' + argv[2];
+const movieId = process.argv[2];
+const filmEndPoint = 'https://swapi-api.hbtn.io/api/films/' + movieId;
+let people = [];
+const names = [];
 
-request(url, async (error, response, body) => {
-  if (error) {
-    console.log(error);
+const requestCharacters = async () => {
+  await new Promise(resolve => request(filmEndPoint, (err, res, body) => {
+    if (err || res.statusCode !== 200) {
+      console.error('Error: ', err, '| StatusCode: ', res.statusCode);
+    } else {
+      const jsonBody = JSON.parse(body);
+      people = jsonBody.characters;
+      resolve();
+    }
+  }));
+};
+
+const requestNames = async () => {
+  if (people.length > 0) {
+    for (const p of people) {
+      await new Promise(resolve => request(p, (err, res, body) => {
+        if (err || res.statusCode !== 200) {
+          console.error('Error: ', err, '| StatusCode: ', res.statusCode);
+        } else {
+          const jsonBody = JSON.parse(body);
+          names.push(jsonBody.name);
+          resolve();
+        }
+      }));
+    }
   } else {
-    for (const value of JSON.parse(body).characters) {
-      const res = await Makerequest(value);
-      console.log(res);
+    console.error('Error: Got no Characters for some reason');
+  }
+};
+
+const getCharNames = async () => {
+  await requestCharacters();
+  await requestNames();
+
+  for (const n of names) {
+    if (n === names[names.length - 1]) {
+      process.stdout.write(n);
+    } else {
+      process.stdout.write(n + '\n');
     }
   }
-});
+};
 
-function Makerequest (url) {
-  return new Promise((resolve, reject) => {
-    request(url, (err, response, body) => {
-      if (err) {
-        console.log(err);
-      } else {
-        resolve(JSON.parse(body).name);
-      }
-    });
-  });
-}
+getCharNames();
